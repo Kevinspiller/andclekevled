39792dcdb13958f7b626c1a504d8f9ac8ff89bef
AndreHofer <andreluizhofer@gmail.com>
2016-06-23 22:12:01 -0300

    deu

--- Fonte/interface/parser.c
+++ Fonte/interface/parser.c
@@ -237,7 +237,7 @@ int interface() {
                                 printf("WARNING: Nothing to be inserted. Command ignored.\n");
                             break;
                         case OP_SELECT_ALL:
-                            imprime(GLOBAL_DATA.objName, NULL); // passado parametro null para descartar o parametro do nome da coluna na função imprime em sqlcommands.c
+                            imprime(GLOBAL_DATA.objName, GLOBAL_DATA.columnName); // passado parametro null para descartar o parametro do nome da coluna na função imprime em sqlcommands.c
                             break;
 						case OP_SELECT_WHERE:
 							imprime(GLOBAL_DATA.objName, GLOBAL_DATA.columnName);
